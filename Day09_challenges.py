a637ae5e3abc3c598bb372c2ee0e5d347ec041f1
life-jouuu <95230736+life-jouuu@users.noreply.github.com>
2024-01-20 19:54:12 +0800

    linechart

---
+++ Day09_challenges.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header('Line chart')
+chart_data = pd.DataFrame(np.random.randn(30,3),columns=['a','b','c'])
+st.line_chart(chart_data)
